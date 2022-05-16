63ef3615c1aaea48d42c603beef32244ccd374d1
lerouzicjulien <lerouzicjulien@gmail.com>
2022-05-16 09:20:31 +0200

    install react model

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      <img src={reactLogo} alt="react logo" />
+      <h1>Composant : App</h1>
+    </div>
+  );
+}
+
+// == Export
+export default App;
